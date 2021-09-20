31d4b03673fdb4f91a4a23af7b205d8128b2eab1
leonardobarone <leonardobarone26@gmail.com>
2021-09-20 15:11:35 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// Descrizione:
+// Rifare l'esercizio della to do list come fatto assieme in classe:
+// - stampare in pagina un item per ogni elemento contenuto in un array
+// - ogni item ha una "x" associata: cliccando su di essa, l'item viene rimosso dalla lista
+// - predisporre un input per aggiungere un nuovo item alla lista: digitando il tasto invio oppure ciccando su un pulsante, il testo digitato viene aggiunto alla lista
+// Bonus:
+// Aggiungere lo stato per ogni todo e modificare la visualizzazione dell'elemento in pagina, verde e barrato se il todo è stato fatto o rosso se non è ancora stato fatto.
+// Prevedere inoltre un pulsante per modificare lo stato del singolo todo
+
+// font -> font-family: 'Lato', sans-serif;
